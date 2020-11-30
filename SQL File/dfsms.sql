b43a7a7c3dc3f139d4295357a30d52d9852c17fa
Adnan Shaikh <adnanamjadshaikh@gmail.com>
2020-11-30 14:48:41 +0530

    database change

--- SQL File/dfsms.sql
+++ SQL File/dfsms.sql
@@ -1,32 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 25, 2020 a1 09:56 PM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dfsms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbladmin`
---
 
 CREATE TABLE `tbladmin` (
   `ID` int(5) NOT NULL,
@@ -39,18 +15,11 @@ CREATE TABLE `tbladmin` (
   `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tbladmin`
---
 
 INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`, `UpdationDate`) VALUES
 (1, 'Admin', 'admin', 1234567899, 'admin@test.com', 'f925916e2754e5e03f75dd58a5733251', '2020-11-29 18:30:00', '2020-11-29 14:56:18');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `tblcategory`
---
 
 CREATE TABLE `tblcategory` (
   `id` int(11) NOT NULL,
@@ -59,9 +28,6 @@ CREATE TABLE `tblcategory` (
   `PostingDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblcategory`
---
 
 INSERT INTO `tblcategory` (`id`, `CategoryName`, `CategoryCode`, `PostingDate`) VALUES
 (1, 'Milk', 'MK01', '2020-11-29 16:27:43'),
@@ -71,11 +37,6 @@ INSERT INTO `tblcategory` (`id`, `CategoryName`, `CategoryCode`, `PostingDate`) 
 (5, 'Soya', 'SY01', '2020-11-29 16:29:58'),
 (6, 'Ghee', 'GH01', '2020-11-29 14:52:08');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tblcompany`
---
 
 CREATE TABLE `tblcompany` (
   `id` int(11) NOT NULL,
@@ -83,9 +44,6 @@ CREATE TABLE `tblcompany` (
   `PostingDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblcompany`
---
 
 INSERT INTO `tblcompany` (`id`, `CompanyName`, `PostingDate`) VALUES
 (1, 'Amul', '2020-11-29 03:30:51'),
@@ -94,11 +52,6 @@ INSERT INTO `tblcompany` (`id`, `CompanyName`, `PostingDate`) VALUES
 (4, 'Namaste India', '2020-11-29 03:31:21'),
 (5, 'Paras', '2020-11-29 14:52:50');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tblorders`
---
 
 CREATE TABLE `tblorders` (
   `id` int(11) NOT NULL,
@@ -111,9 +64,6 @@ CREATE TABLE `tblorders` (
   `InvoiceGenDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblorders`
---
 
 INSERT INTO `tblorders` (`id`, `ProductId`, `Quantity`, `InvoiceNumber`, `CustomerName`, `CustomerContactNo`, `PaymentMode`, `InvoiceGenDate`) VALUES
 (1, 4, 2, 753947547, 'Anuj', 9354778033, 'cash', '2020-11-29 08:32:47'),
@@ -129,11 +79,6 @@ INSERT INTO `tblorders` (`id`, `ProductId`, `Quantity`, `InvoiceNumber`, `Custom
 (11, 7, 1, 139640585, 'John', 45632147892, 'cash', '2020-11-29 14:54:24'),
 (12, 5, 1, 139640585, 'John', 45632147892, 'cash', '2020-11-29 14:54:24');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tblproducts`
---
 
 CREATE TABLE `tblproducts` (
   `id` int(11) NOT NULL,
@@ -145,9 +90,6 @@ CREATE TABLE `tblproducts` (
   `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblproducts`
---
 
 INSERT INTO `tblproducts` (`id`, `CategoryName`, `CompanyName`, `ProductName`, `ProductPrice`, `PostingDate`, `UpdationDate`) VALUES
 (1, 'Milk', 'Amul', 'Toned milk 500ml', '22', '2020-11-29 05:22:37', '2020-11-29 05:22:37'),
@@ -158,75 +100,43 @@ INSERT INTO `tblproducts` (`id`, `CategoryName`, `CompanyName`, `ProductName`, `
 (6, 'Bread', 'Patanjali', 'Sandwich Bread', '30', '2020-11-29 11:40:10', NULL),
 (7, 'Ghee', 'Paras', 'Ghee 500mg', '350', '2020-11-29 14:53:33', '2020-11-29 14:53:33');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tbladmin`
---
 ALTER TABLE `tbladmin`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indexes for table `tblcategory`
---
+
 ALTER TABLE `tblcategory`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tblcompany`
---
+
 ALTER TABLE `tblcompany`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tblorders`
---
+
 ALTER TABLE `tblorders`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tblproducts`
---
+
 ALTER TABLE `tblproducts`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tbladmin`
---
 ALTER TABLE `tbladmin`
   MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `tblcategory`
---
+
 ALTER TABLE `tblcategory`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT for table `tblcompany`
---
+
 ALTER TABLE `tblcompany`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `tblorders`
---
+
 ALTER TABLE `tblorders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- AUTO_INCREMENT for table `tblproducts`
---
+
 ALTER TABLE `tblproducts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
